4227c7e6ce91a4827ccb8f681cb5184505a5e963
Robin <robin@lidbetter.me>
2023-11-04 22:14:00 -0400

    rename stuff

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,7 +3,7 @@
 # docker pull registry.gitlab.steamos.cloud/steamrt/scout/sdk
 # docker run -v D:\repos:/dockerVolume -it <imageID>
 
-export HL2SDKCS2='/dockerVolume/mm-cs2-scrim/sdk'
+export HL2SDKCS2='/dockerVolume/cs2-prac/sdk'
 export MMSOURCE_DEV='/dockerVolume/metamod-source'
 export CC=clang
 export CXX=clang++
@@ -21,7 +21,7 @@ apt-get install gcc -y
 cd "/dockerVolume/ambuild"
 python3 setup.py install
 
-cd "/dockerVolume/mm-cs2-scrim"
+cd "/dockerVolume/cs2-prac"
 mkdir build
 cd build
 
